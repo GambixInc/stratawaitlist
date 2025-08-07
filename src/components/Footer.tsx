14467dbd30d29bf9d89cae76c5fb2aaf405f3ae8
Ravi Gupta <ravgupta20@gmail.com>
2025-08-07 10:13:21 -0400

    updated foot's private policy date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="container mx-auto px-4 py-4 md:py-6">
       <div className="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0 text-white/50 text-xs md:text-sm">
         <PolicyDialogs />
-        <p>©2024 Gambix LLC. All Rights Reserved.</p>
+        <p>©2025 Gambix LLC. All Rights Reserved.</p>
       </div>
     </footer>
   );
